17ed436e7d7650bdfccef6d5705ee7049ac11de7
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-08-06 19:12:30 +0200

    Update ImmediateElementAt.cs

--- src/ExtraLinq/Immediate/ImmediateElementAt.cs
+++ src/ExtraLinq/Immediate/ImmediateElementAt.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+
 using ExtraLinq.Internals.Localizations;
 
 namespace ExtraLinq.Immediate;
@@ -35,30 +36,5 @@ public static class ImmediateElementAt
 
         throw new ArgumentException(Resources.Exceptions_ValueNotFound_AtIndex.Replace("{y}", nameof(source))
             .Replace("{x}",$"{index}"));
-    }
-    
-    /// <summary>
-    /// Returns a IList of elements from the specified source, 
-    /// where the index of each element in the returned IList corresponds to an index in the provided indexes.
-    /// </summary>
-    /// <remarks>The order of the elements in the returned IList is determined by their original position in the source,
-    /// but the order within the returned IList is based on the provided indexes.</remarks>
-    /// <param name="source">The IList from which to retrieve elements.</param>
-    /// <param name="indices">A sequence of indices, where each index corresponds to an element in the source.</param>
-    /// <typeparam name="T">The type of the elements in the source and returned IList.</typeparam>
-    /// <returns>A new IList containing the elements at the specified indexes from the original source.</returns>
-    public static IList<T> ElementsAt<T>(this IList<T> source, IList<int> indices)
-    {
-        List<T> output = new();
-        
-        foreach (var index in indices)
-        {
-            if (index >= 0 && index < source.Count)
-            {
-                output.Add(source[index]);
-            }
-        }
-            
-        return output;
     }
 }
