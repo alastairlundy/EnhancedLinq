e5752d4e9e556918149e5799b03996b883300dbf
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-08-06 16:55:16 +0200

    move Segment non predicate extension methods

--- src/ExtraLinq.MsExtensions/Immediate/StringSegments/ImmediateSegmentFirst.cs
+++ src/ExtraLinq.MsExtensions/Immediate/StringSegments/ImmediateSegmentFirst.cs
@@ -11,20 +11,6 @@ namespace ExtraLinq.MsExtensions.Immediate.StringSegments;
 
 public static class ImmediateSegmentFirst
 {
-    
-    /// <summary>
-    /// Returns the first char in the StringSegment.
-    /// </summary>
-    /// <param name="target">The StringSegment to be searched.</param>
-    /// <returns>The first char in the StringSegment.</returns>
-    /// <exception cref="InvalidOperationException">Thrown if the StringSegment contains zero chars.</exception>
-    public static char First(this StringSegment target)
-    {
-        if (target.IsEmpty())
-            throw new InvalidOperationException(Resources.Exceptions_Enumerables_InvalidOperation_EmptySequence);
-        
-        return target[0];
-    }
 
     /// <summary>
     /// Returns the first char in the StringSegment that matches the predicate condition.
@@ -47,18 +33,7 @@ public static class ImmediateSegmentFirst
         throw new ArgumentException(Resources.Exceptions_StringSegment_NoPredicateMatches);
     }
 
-    /// <summary>
-    /// Returns the first character of the specified <see cref="StringSegment"/> or null if the segment is empty.
-    /// </summary>
-    /// <param name="target">The <see cref="StringSegment"/> from which to retrieve the first character.</param>
-    /// <returns>The first character of the segment if it exists; otherwise, null.</returns>
-    public static char? FirstOrDefault(this StringSegment target)
-    {
-        if (StringSegment.IsNullOrEmpty(target))
-            return default;
 
-        return target[0];
-    }
 
     /// <summary>
     /// Returns the first character of the specified <see cref="StringSegment"/> that meets the predicate condition or null if the segment is empty.
