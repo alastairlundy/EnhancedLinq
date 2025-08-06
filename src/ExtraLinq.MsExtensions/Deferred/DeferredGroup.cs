b808c594e17371dd084d507cba044175b6f16734
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-08-06 17:35:56 +0200

    implement StringSegment GroupBy

--- src/ExtraLinq.MsExtensions/Deferred/DeferredGroup.cs
+++ src/ExtraLinq.MsExtensions/Deferred/DeferredGroup.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+
 using ExtraLinq.MsExtensions.Deferred.Enumerables;
+
 using Microsoft.Extensions.Primitives;
 
 namespace ExtraLinq.MsExtensions.Deferred;
@@ -9,13 +11,19 @@ namespace ExtraLinq.MsExtensions.Deferred;
 public static class DeferredGroup
 {
     
-    
-    public static IGrouping<TKey, char> GroupBy<TKey>(this StringSegment target, 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="target"></param>
+    /// <param name="selector"></param>
+    /// <typeparam name="TKey"></typeparam>
+    /// <returns></returns>
+    /// <exception cref="ArgumentNullException"></exception>
+    public static IEnumerable<IGrouping<TKey, char>> GroupBy<TKey>(this StringSegment target, 
         Func<char, TKey> selector)
     {
-        StringSegment.IsNullOrEmpty(nameof(target));
-        
-        
+        if(StringSegment.IsNullOrEmpty(target))
+            throw new ArgumentNullException(nameof(target));
         
         return new GroupStringSegmentEnumerable<TKey>(target, selector);
     }
--- src/ExtraLinq.MsExtensions/Deferred/Enumerators/GroupStringSegmentEnumerator.cs
+++ src/ExtraLinq.MsExtensions/Deferred/Enumerators/GroupStringSegmentEnumerator.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+
 using AlastairLundy.DotPrimitives.Collections.Groupings;
+
 using Microsoft.Extensions.Primitives;
 
 namespace ExtraLinq.MsExtensions.Deferred.Enumerators;
@@ -12,9 +14,15 @@ public class GroupStringSegmentEnumerator<TKey> : IEnumerator<IGrouping<TKey, ch
     private readonly StringSegment _source;
     private readonly Func<char, TKey> _selector;
 
+    private IEnumerator<char> _enumerator;
+    
+    private TKey _currentKey;
+    
     private int _state;
 
     private GroupingCollection<TKey, char>  _groupingCollection;
+
+    private IGrouping<TKey, char> _currentGrouping;
     
     internal GroupStringSegmentEnumerator(StringSegment source, Func<char, TKey> selector)
     {
@@ -27,14 +35,49 @@ public class GroupStringSegmentEnumerator<TKey> : IEnumerator<IGrouping<TKey, ch
     {
         if (_state == 1)
         {
-            
+            _enumerator = new SegmentEnumerator(_source);
         }
 
         if (_state == 2)
         {
-            
+            try
+            {
+                while(_enumerator.MoveNext())
+                {
+                    if (_currentKey is not null && _currentKey.Equals(default(TKey)))
+                    {
+                        _currentKey = _selector(_enumerator.Current);
+                    }
+                    
+                    TKey key = _selector(_enumerator.Current);
+                    
+                    if (key is not null && key.Equals(_currentKey))
+                    {
+                        _groupingCollection.Add(_enumerator.Current);
+                    }
+                    else
+                    {
+                        _currentGrouping = new GroupingCollection<TKey, char>
+                            (_currentKey, _groupingCollection, false);
+                        
+                        _groupingCollection.Clear();
+
+                        _currentKey = default(TKey);
+                        
+                        return true;
+                    }
+                }
+            }
+            catch
+            {
+                Dispose();
+                throw;
+            }
+
+            _state = -1;
         }
-        
+
+        return false;
     }
 
     public void Reset()
