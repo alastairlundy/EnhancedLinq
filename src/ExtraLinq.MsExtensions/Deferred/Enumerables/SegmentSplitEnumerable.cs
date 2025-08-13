4037006ba1a5031db76620fe3e09c3d46d86536e
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-08-13 22:50:00 +0200

    Create SegmentSplitEnumerable.cs

---
+++ src/ExtraLinq.MsExtensions/Deferred/Enumerables/SegmentSplitEnumerable.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using AlastairLundy.DotExtensions.MsExtensions.System.StringSegments;
+
+using ExtraLinq.MsExtensions.Deferred.Enumerators;
+
+using Microsoft.Extensions.Primitives;
+
+namespace ExtraLinq.MsExtensions.Deferred.Enumerables;
+
+public class SegmentSplitEnumerable : IEnumerable<StringSegment>
+{
+    private readonly StringSegment _segment;
+    private readonly StringSegment _separator;
+
+    internal SegmentSplitEnumerable(StringSegment segment,StringSegment separator)
+    {
+        _segment = segment;
+        _separator = separator;
+    }
+    
+    public IEnumerator<StringSegment> GetEnumerator()
+    {
+        
+        if(_separator.Length == 1)
+            return new SegmentSplitCharEnumerator(_segment, _separator.First());
+        else
+            return new SegmentSplitEnumerator(_segment, _separator);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
