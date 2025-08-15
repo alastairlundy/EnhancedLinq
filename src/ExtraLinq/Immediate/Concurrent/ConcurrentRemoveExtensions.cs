37e23d8b3723245f59fd33fe47587cda25a55188
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-08-15 11:08:00 +0200

    remove unneeded classes

---
+++ src/ExtraLinq/Immediate/Concurrent/ConcurrentRemoveExtensions.cs
@@ -0,0 +1,37 @@
+﻿/*
+    ExtraLinq 
+    Copyright (c) 2025 Alastair Lundy
+    
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExtraLinq.Immediate.Concurrent;
+
+/// <summary>
+/// 
+/// </summary>
+public static class ConcurrentRemoveExtensions
+{
+    
+    /// <summary>
+    /// Removes all occurrences of a specified object from a concurrent bag.
+    /// </summary>
+    /// <param name="concurrentBag">The concurrent bag to remove objects from.</param>
+    /// <param name="obj">The object to be removed from the concurrent bag.</param>
+    /// <typeparam name="T">The type of elements contained within the concurrent bag.</typeparam>
+    /// <returns>A new concurrent bag with all occurrences of the specified object removed.</returns>
+    public static ConcurrentBag<T> Remove<T>(this ConcurrentBag<T> concurrentBag, T obj)
+    {
+        IEnumerable<T> newCollection = from item in concurrentBag
+            where item.Equals(obj) == false
+            select item;
+        
+        return new ConcurrentBag<T>(newCollection);
+    }
+}
