fc7dbf07661322b9d865646779bfa59faae3e564
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-08-15 11:40:00 +0200

    add missing xml doc comments

--- src/ExtraLinq.Memory/Immediate/Ranges/ImmediateRemoveRange.cs
+++ src/ExtraLinq.Memory/Immediate/Ranges/ImmediateRemoveRange.cs
@@ -12,12 +12,12 @@ namespace ExtraLinq.Memory.Immediate.Ranges;
 public static partial class ExtraLinqMemoryImmediateRange
 {
     /// <summary>
-    /// 
+    /// Creates a new Span with all items of the original Span minus the items to be removed.
     /// </summary>
-    /// <param name="target"></param>
-    /// <param name="indices"></param>
-    /// <typeparam name="T"></typeparam>
-    /// <returns></returns>
+    /// <param name="target">The span to remove a range of items from.</param>
+    /// <param name="indices">The indices of the items to be removed.</param>
+    /// <typeparam name="T">The type of elements in the span.</typeparam>
+    /// <returns>A new Span with all items of the original Span minus the items to be removed.</returns>
     public static Span<T> RemoveRange<T>(this Span<T> target, ICollection<int> indices) where T : IEquatable<T>?
     {
         T[] elements = target.GetRange(indices)
@@ -29,29 +29,28 @@ public static partial class ExtraLinqMemoryImmediateRange
     }
     
     /// <summary>
-    /// 
+    /// Creates a new Span with all items of the original Span minus the items to be removed.
     /// </summary>
-    /// <param name="target"></param>
-    /// <param name="range"></param>
-    /// <typeparam name="T"></typeparam>
-    /// <returns></returns>
+    /// <param name="target">The span to remove a range of items from.</param>
+    /// <param name="range">The index <see cref="Range"/> of items to be removed.</param>
+    /// <typeparam name="T">The type of elements in the span.</typeparam>
+    /// <returns>A new Span with all items of the original Span minus the items to be removed.</returns>
     public static Span<T> RemoveRange<T>(this Span<T> target, Range range)
         => RemoveRange(target, range.Start.Value, range.End.Value);
 
     /// <summary>
-    /// 
+    /// Creates a new Span with all items of the original Span minus the items to be removed.
     /// </summary>
-    /// <param name="target"></param>
-    /// <param name="startIndex"></param>
-    /// <param name="count"></param>
-    /// <typeparam name="T"></typeparam>
-    /// <returns></returns>
+    /// <param name="target">The span to remove a range of items from.</param>
+    /// <param name="startIndex">The zero-based index to start removing items at.</param>
+    /// <param name="count">The number of items to remove from the Span from the start index.</param>
+    /// <typeparam name="T">The type of elements in the span.</typeparam>
+    /// <returns>A new Span with all items of the original Span minus the items to be removed.</returns>
     public static Span<T> RemoveRange<T>(this Span<T> target, int startIndex, int count)
     {
         if (target.IsEmpty)
             throw new ArgumentException();
-
-
+        
         if (startIndex < 0 || startIndex > target.Length)
             throw new IndexOutOfRangeException();
         
