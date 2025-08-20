398a15d097526ace7d1b9ec758b2df6a0535e6ef
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-08-20 11:36:39 +0100

    add StringSegment AsEnumerable

---
+++ src/ExtraLinq.MsExtensions/Deferred/Enumerables/SegmentEnumerable.cs
@@ -0,0 +1,34 @@
+/*
+    ExtraLinq 
+    Copyright (c) 2025 Alastair Lundy
+    
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+
+using ExtraLinq.MsExtensions.Deferred.Enumerators;
+
+using Microsoft.Extensions.Primitives;
+
+namespace ExtraLinq.MsExtensions.Deferred.Enumerables;
+
+internal class SegmentEnumerable : IEnumerable<char>
+{
+    private readonly StringSegment _segment;
+
+    internal SegmentEnumerable(StringSegment segment)
+    {
+        _segment = segment;
+    }
+    
+    public IEnumerator<char> GetEnumerator()
+    {
+        return new SegmentEnumerator(_segment);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
