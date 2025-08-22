ecc1a47faf0436d3d53283d4211e71405acb568b
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-08-22 13:46:42 +0100

    clean up type name

--- src/EnhancedLinq/Deferred/Ranges/DeferredRemoveRange.cs
+++ src/EnhancedLinq/Deferred/Ranges/DeferredRemoveRange.cs
@@ -19,9 +19,9 @@ public static partial class EnhancedLinqDeferredRange
     /// </summary>
     /// <param name="source">The IEnumerable to have items removed from.</param>
     /// <param name="itemsToBeRemoved">The items to be removed.</param>
-    /// <typeparam name="T">The type of elements stored in the IEnumerable.</typeparam>
+    /// <typeparam name="TSource">The type of elements stored in the IEnumerable.</typeparam>
     /// <returns>The new IEnumerable with the specified items removed.</returns>
-    public static IEnumerable<T> RemoveRange<T>(this IEnumerable<T> source, IEnumerable<T> itemsToBeRemoved)
+    public static IEnumerable<TSource> RemoveRange<TSource>(this IEnumerable<TSource> source, IEnumerable<TSource> itemsToBeRemoved)
     {
         return from item in source
             where itemsToBeRemoved.Contains(item) == false
