d8ea2e90112052d4ca63c084bffb11bed109254d
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-08-22 11:09:26 +0100

    rework Last Predicate to be more efficient

--- src/EnhancedLinq.MsExtensions/StringSegments/Immediate/ImmediateSegmentLastPredicate.cs
+++ src/EnhancedLinq.MsExtensions/StringSegments/Immediate/ImmediateSegmentLastPredicate.cs
@@ -8,6 +8,9 @@
  */
 
 using System;
+
+using EnhancedLinq.MsExtensions.Internals.Localizations;
+
 using Microsoft.Extensions.Primitives;
 
 namespace EnhancedLinq.MsExtensions.StringSegments.Immediate;
@@ -22,8 +25,19 @@ public static class ImmediateSegmentLastPredicate
     /// <param name="predicate">The predicate func condition to be checked against each char in the StringSegment.</param>
     /// <returns>The last character of the segment that meets the predicate condition if any match.</returns>
     /// <exception cref="ArgumentException">Thrown if no characters in the StringSegment meet the predicate condition.</exception>
-    public static char Last(this StringSegment target, Func<char, bool> predicate) => 
-        target.Reverse().First(predicate);
+    public static char Last(this StringSegment target, Func<char, bool> predicate)
+    {
+        if(StringSegment.IsNullOrEmpty(target))
+            throw new InvalidOperationException(Resources.Exceptions_Segments_InvalidOperation_EmptySequence);
+        
+        for (int i = target.Length - 1; i >= 0; i--)
+        {
+            if(predicate(target[i]))
+                return target[i];
+        }
+
+        throw new ArgumentException();
+    }
 
     /// <summary>
     /// Returns the last character of the specified <see cref="StringSegment"/> that matches the predicate condition or a default value if the segment is empty.
@@ -31,6 +45,17 @@ public static class ImmediateSegmentLastPredicate
     /// <param name="target">The StringSegment to be searched.</param>
     /// <param name="predicate">The predicate func condition to be checked against each char in the StringSegment.</param>
     /// <returns>The last character of the segment that meets the predicate condition if any match; otherwise, null.</returns>
-    public static char? LastOrDefault(this StringSegment target, Func<char, bool> predicate) 
-        => target.Reverse().FirstOrDefault(predicate);
+    public static char? LastOrDefault(this StringSegment target, Func<char, bool> predicate)
+    {
+        if(StringSegment.IsNullOrEmpty(target))
+            throw new InvalidOperationException(Resources.Exceptions_Segments_InvalidOperation_EmptySequence);
+        
+        for (int i = target.Length - 1; i >= 0; i--)
+        {
+            if(predicate(target[i]))
+                return target[i];
+        }
+
+        return null;
+    }
 }
