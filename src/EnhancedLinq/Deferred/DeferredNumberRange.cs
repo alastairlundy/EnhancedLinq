df70ed288437ce0cff970c048ea9f8d856ebd431
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-08-31 16:51:31 +0100

    improve code comments

--- src/EnhancedLinq/Deferred/DeferredNumberRange.cs
+++ src/EnhancedLinq/Deferred/DeferredNumberRange.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
+
 using AlastairLundy.EnhancedLinq.Deferred.Enumerables.NumberRanges;
 
 // ReSharper disable ConvertClosureToMethodGroup
@@ -19,16 +20,17 @@ namespace AlastairLundy.EnhancedLinq.Deferred;
 
 public static partial class EnhancedLinqDeferred
 {
-    
+
     /// <summary>
     /// Generates a sequence of numeric values starting from a specified value and continuing for a specified count,
     /// with each value incremented by 1 from the starting point.
     /// </summary>
     /// <param name="start">The starting value of the sequence.</param>
     /// <param name="count">The number of values to generate in the sequence.</param>
-    /// <returns>An <see cref="IEnumerable{T}"/> containing the generated sequence of unsigned long values,
-    /// incremented by 1 from the starting point.</returns>
-    /// <typeparam name="TNumber"></typeparam>
+    /// <param name="incrementor">The amount to increment each number by.</param>
+    /// <returns>A sequence containing the generated numeric values,
+    /// incremented by the incrementor amount from the starting point.</returns>
+    /// <typeparam name="TNumber">The numeric type used to represent the numbers.</typeparam>
     /// <exception cref="ArgumentException">Thrown if the start number or count are NaN.</exception>
     /// <exception cref="NotFiniteNumberException">Thrown if the start number or count are infinity.</exception>
     public static IEnumerable<TNumber> GenerateNumberRange<TNumber>(this TNumber start, TNumber count)
@@ -44,14 +46,17 @@ public static partial class EnhancedLinqDeferred
     }
 
     /// <summary>
-    /// 
+    /// Generates a sequence of numeric values starting from a specified value and continuing for a specified count, skipping, 
+    /// with each value incremented by 1 from the starting point.
     /// </summary>
     /// <param name="start">The starting value of the sequence.</param>
     /// <param name="count">The number of values to generate in the sequence.</param>
-    /// <param name="numbersToSkip"></param>
-    /// <typeparam name="TNumber"></typeparam>
-    /// <returns></returns>
     public static IEnumerable<TNumber> GenerateNumberRange<TNumber>(this TNumber start, TNumber count,
+    /// <param name="incrementor">The amount to increment each number by.</param>
+    /// <param name="numbersToSkip">The numbers to skip when generating the range of numbers.</param>
+    /// <typeparam name="TNumber">The numeric type used to represent the numbers.</typeparam>
+    /// <returns>A sequence containing the generated numeric values,
+    /// incremented by the incrementor amount from the starting point.</returns>
         IEnumerable<TNumber> numbersToSkip) where TNumber : INumber<TNumber> 
         => GenerateNumberRange(start, count)
             .SkipWhile(x => numbersToSkip.Contains(x));
