0cebf44da33e45a7085a16b9fc0a9a7c41b1b9b9
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-09-06 19:25:45 +0100

    improve LastIndexOf ICollection<T> performance

--- src/EnhancedLinq/Immediate/ImmediateLastIndexOf.cs
+++ src/EnhancedLinq/Immediate/ImmediateLastIndexOf.cs
@@ -17,27 +17,63 @@ public static partial class EnhancedLinqImmediate
 {
     
     /// <summary>
-    /// Gets the first index of the last element that matches the predicate condition.
+    /// Gets the index of the last element that matches the predicate condition.
     /// </summary>
+    /// <remarks>
+    /// This method is a computationally expensive as the number of items in the sequence is needed, to obtain the index
+    /// of the last element that satisfies the selector.
+    /// </remarks>
     /// <param name="source">The <see cref="IEnumerable{T}"/> to be searched.</param>
     /// <param name="selector">The predicate condition to check elements of the sequence against.</param>
-    /// <typeparam name="T">The type of elements in the IEnumerable.</typeparam>
-    /// <returns>The first index of the last element in the sequence to match the predicate condition,
+    /// <typeparam name="T">The type of elements in the sequence.</typeparam>
+    /// <returns>The index of the last element in the sequence to match the predicate condition,
     /// if the sequence contains any elements that match the predicate condition, returns -1 otherwise.
     /// </returns>
-    /// <exception cref="ArgumentNullException"></exception>
     public static int LastIndexOf<T>(this IEnumerable<T> source, Func<T, bool> selector)
     {
         ArgumentNullException.ThrowIfNull(source, nameof(source));
 
-        IList<T> list = source as IList<T> ?? source.ToArray();
+        if (source is ICollection<T> collection)
+        {
+            return LastIndexOf(collection, selector);
+        }
         
-        if(list is null)
-            throw new  ArgumentNullException(nameof(source));
+        bool foundItem = false;
+        int reverseIndex = 0;
+        
+        int count = 0;
+        
+        foreach (T item in source.Reverse())
+        {
+            if (selector(item))
+            {
+                foundItem = true;
+                reverseIndex = count;
+            }
 
-        int index = list.Count - 1;
+            count++;
+        }
+
         
-        foreach (T item in list.Reverse())
+        return foundItem ? Math.Abs(count - reverseIndex) : -1;
+    }
+    
+    /// <summary>
+    /// Gets the index of the last element that matches the predicate condition.
+    /// </summary>
+    /// <param name="source">The <see cref="ICollection{T}"/> to be searched.</param>
+    /// <param name="selector">The predicate condition to check elements of the collection against.</param>
+    /// <typeparam name="T">The type of elements in the collection.</typeparam>
+    /// <returns>The index of the last element in the collection to match the predicate condition,
+    /// if the collection contains any elements that match the predicate condition, returns -1 otherwise.
+    /// </returns>
+    public static int LastIndexOf<T>(this ICollection<T> source, Func<T, bool> selector)
+    {
+        ArgumentNullException.ThrowIfNull(source, nameof(source));
+        
+        int index = source.Count - 1;
+        
+        foreach (T item in source.Reverse())
         {
             if(selector(item))
                 return index;
@@ -47,27 +83,54 @@ public static partial class EnhancedLinqImmediate
 
         return -1;
     }
-
+    
     /// <summary>
     /// Gets the last index of an element in a sequence.
     /// </summary>
+    /// <remarks>
+    /// This method is a computationally expensive as the number of items in the sequence is needed, to obtain the index
+    /// of the last element that satisfies the selector.
+    /// </remarks>
     /// <param name="source">The sequence to be searched.</param>
-    /// <param name="obj">The element to get the index of.</param>
+    /// <param name="obj">The element to get the last index of.</param>
     /// <typeparam name="T">The type of elements in the sequence.</typeparam>
     /// <returns>The last index of an element in a sequence, if the sequence contains the element, returns -1 otherwise.</returns>
-    /// <exception cref="ArgumentNullException"></exception>
     public static int LastIndexOf<T>(this IEnumerable<T> source, T obj)
     {
         ArgumentNullException.ThrowIfNull(source, nameof(source));
-
-        IList<T> list = source as IList<T> ?? source.ToArray();
         
-        if(list is null)
-            throw new  ArgumentNullException(nameof(source));
-        
-        int index = list.Count -1;
+        bool foundItem = false;
+        int reverseIndex = 0;
+        int count = 0;
                 
-        foreach (T item in list.Reverse())
+        foreach (T item in source.Reverse())
+        {
+            if (item is not null && item.Equals(obj))
+            {
+                foundItem = true;
+                reverseIndex = count;
+            }
+                    
+            count++;
+        }
+        
+        return foundItem ? Math.Abs(count - reverseIndex) : -1;
+    }
+
+    /// <summary>
+    /// Gets the last index of an element in a collection.
+    /// </summary>
+    /// <param name="source">The collection to be searched.</param>
+    /// <param name="obj">The element to get the index of.</param>
+    /// <typeparam name="T">The type of elements in the collection.</typeparam>
+    /// <returns>The index of an element in a collection, if the collection contains the element, returns -1 otherwise.</returns>
+    public static int LastIndexOf<T>(this ICollection<T> source, T obj)
+    {
+        ArgumentNullException.ThrowIfNull(source, nameof(source));
+        
+        int index = source.Count -1;
+                
+        foreach (T item in source.Reverse())
         {
             if (item is not null && item.Equals(obj))
                 return index;
