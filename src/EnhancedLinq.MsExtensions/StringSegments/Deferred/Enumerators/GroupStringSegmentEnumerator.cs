2a255a67588b1eba84c17c54b7c6d98f9e503991
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-09-15 16:12:04 +0100

    Update GroupStringSegmentEnumerator.cs

--- src/EnhancedLinq.MsExtensions/StringSegments/Deferred/Enumerators/GroupStringSegmentEnumerator.cs
+++ src/EnhancedLinq.MsExtensions/StringSegments/Deferred/Enumerators/GroupStringSegmentEnumerator.cs
@@ -11,7 +11,9 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+
 using AlastairLundy.DotPrimitives.Collections.Groupings;
+
 using Microsoft.Extensions.Primitives;
 
 namespace AlastairLundy.EnhancedLinq.MsExtensions.StringSegments.Deferred.Enumerators;
