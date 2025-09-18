30d5d0a72aedf2273ad57c5b7c4a846edce22a03
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-09-18 22:07:05 +0100

    add Collection InsertRange

--- src/EnhancedLinq/Immediate/Lists/Ranges/ImmediateListInsertRange.cs
+++ src/EnhancedLinq/Immediate/Lists/Ranges/ImmediateListInsertRange.cs
@@ -9,12 +9,51 @@
 
 using System;
 using System.Collections.Generic;
+
+using AlastairLundy.DotExtensions.Collections;
+
+using AlastairLundy.EnhancedLinq.Immediate.Linq;
 using AlastairLundy.EnhancedLinq.Internals.Localizations;
+
+using EnhancedLinq.Immediate.Ranges;
 
 namespace AlastairLundy.EnhancedLinq.Immediate.Ranges;
 
+/// <summary>
+/// 
+/// </summary>
 public static partial class EnhancedLinqImmediateRange
 {
+    /// <summary>
+    /// Inserts a specified range of elements from another sequence into this collection at a specified position.
+    /// </summary>
+    /// <param name="source">The collection into which to insert the new elements.</param>
+    /// <param name="index">The zero-based index where the new elements will be inserted. If less than 0, values are inserted at the end of the collection.</param>
+    /// <param name="values">The sequence of elements to be inserted into the collection.</param>
+    /// <typeparam name="T">The type of elements in the value sequence and the collection.</typeparam>
+    /// <exception cref="IndexOutOfRangeException">Thrown if the specified index is out of range for this collection.</exception>
+    public static void InsertRange<T>(this ICollection<T> source, int index, IEnumerable<T> values)
+    {
+        ArgumentNullException.ThrowIfNull(source);
+        
+        if (index < 0 || index > source.Count)
+        {
+            throw new IndexOutOfRangeException(Resources.Exceptions_IndexOutOfRange
+                .Replace("{x}", $"{index}")
+                .Replace("{y}", $"0")
+                .Replace("z", $"{source.Count}"));
+        }
+
+        int numberToRemove = source.LastIndex() - index;
+
+        ICollection<T> itemsToRemove = source.Take(numberToRemove);
+        
+        source.RemoveRange(index, numberToRemove);;
+       
+        source.AddRange(values);
+        source.AddRange(itemsToRemove);
+    }
+    
     /// <summary>
     /// Inserts a specified range of elements from another sequence into this list at a specified position.
     /// </summary>
