57d71843b20159dc0673813e7dbb05f23807d1f6
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-09-20 12:47:37 +0100

    add missing license notice

--- src/EnhancedLinq.Memory/Immediate/ImmediateLastIndex.cs
+++ src/EnhancedLinq.Memory/Immediate/ImmediateLastIndex.cs
@@ -1,3 +1,12 @@
+/*
+    EnhancedLinq 
+    Copyright (c) 2025 Alastair Lundy
+    
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 using System;
 
 namespace AlastairLundy.EnhancedLinq.Memory.Immediate;
