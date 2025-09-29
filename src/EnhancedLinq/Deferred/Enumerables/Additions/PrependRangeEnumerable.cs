9e20adb7523010cf3adad1b2b672269549d7d477
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-09-29 10:51:56 +0100

    add PrependRange

---
+++ src/EnhancedLinq/Deferred/Enumerables/Additions/PrependRangeEnumerable.cs
@@ -0,0 +1,54 @@
+/*
+    EnhancedLinq 
+    Copyright (c) 2025 Alastair Lundy
+    
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AlastairLundy.EnhancedLinq.Deferred.Enumerables;
+
+internal class PrependRangeEnumerable<TSource> : IEnumerable<TSource>
+{
+    private readonly IEnumerable<TSource> _source;
+    private readonly IEnumerable<TSource> _toBePrepended;
+
+    internal PrependRangeEnumerable(IEnumerable<TSource> source, IEnumerable<TSource> toBePrepended)
+    {
+        _source = source;
+        _toBePrepended = toBePrepended;
+    }
+    
+    public IEnumerator<TSource> GetEnumerator()
+    {
+        IEnumerator<TSource> sourceEnumerator = _source.GetEnumerator();
+        IEnumerator<TSource> prependEnumerator = _toBePrepended.GetEnumerator();
+        
+        try
+        {
+            while (prependEnumerator.MoveNext())
+            {
+                yield return prependEnumerator.Current;
+            }
+            
+            while (sourceEnumerator.MoveNext())
+            {
+                yield return sourceEnumerator.Current;
+            }
+        }
+        finally
+        {
+            sourceEnumerator.Dispose();
+            prependEnumerator.Dispose();
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
