5c85f89a7da10fe6092feb12c542aa1a8dbee1ca
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-09-29 10:51:06 +0100

    fix xml doc comment issues with AppendRange extension method

--- src/EnhancedLinq/Deferred/Ranges/DeferredAppendRange.cs
+++ src/EnhancedLinq/Deferred/Ranges/DeferredAppendRange.cs
@@ -23,10 +23,10 @@ public static partial class EnhancedLinqDeferredRange
     /// <summary>
     /// Appends one sequence of elements to the end of another specified sequence.
     /// </summary>
-    /// <param name="source">The sequence to add items to.</param>
-    /// <param name="toBeAppended">The elements to add to the sequence.</param>
-    /// <typeparam name="TSource">The type of element in the sequence and elements being added.</typeparam>
-    /// <returns></returns>
+    /// <param name="source">The sequence to append items to.</param>
+    /// <param name="toBeAppended">The elements to append to the sequence.</param>
+    /// <typeparam name="TSource">The type of element in the sequence and elements being appended.</typeparam>
+    /// <returns>A new sequence made up of the source sequence and the appended sequence.</returns>
     public static IEnumerable<TSource> AppendRange<TSource>(this IEnumerable<TSource> source,
         IEnumerable<TSource> toBeAppended)
     { 
