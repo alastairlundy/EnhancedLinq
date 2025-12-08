0201088bbafbfba21d37e4549088be5d70bd2cf1
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-12-08 10:22:21 +0000

    convert extension methods to extension blocks

--- src/EnhancedLinq.Memory/Immediate/Linq/ImmediateSelect.cs
+++ src/EnhancedLinq.Memory/Immediate/Linq/ImmediateSelect.cs
@@ -7,133 +7,123 @@
     file, You can obtain one at https://mozilla.org/MPL/2.0/. 
     */
 
-using System.Diagnostics.CodeAnalysis;
-
 namespace EnhancedLinq.Memory.Immediate;
 
 public static partial class EnhancedLinqMemoryImmediate
 {
-    /// <summary>
-    /// Transforms elements of a Span according to behaviour defined by the predicate.
-    /// </summary>
     /// <param name="source">The span to search.</param>
-    /// <param name="predicate">The predicate to use.</param>
     /// <typeparam name="TSource">The type of elements in the source Span.</typeparam>
-    /// <typeparam name="TResult">The type of elements the predicate transforms elements into.</typeparam>
-    /// <returns>The newly created Span with the elements transformed by the predicate.</returns>
-    public static Span<TResult> Select<TSource, TResult>(
-#if NET8_0_OR_GREATER
-        [NotNull]
-#endif
-        this Span<TSource> source,
-#if NET8_0_OR_GREATER
-        [NotNull]
-#endif
-        Func<TSource, TResult> predicate)
+    extension<TSource>(Span<TSource> source)
     {
-        TResult[] array = new  TResult[source.Length];
-        
-        int index = 0;
-        
-        source.ForEach(x =>
+        /// <summary>
+        /// Transforms elements of a Span according to behaviour defined by the predicate.
+        /// </summary>
+        /// <param name="predicate">The predicate to use.</param>
+        /// <typeparam name="TResult">The type of elements the predicate transforms elements into.</typeparam>
+        /// <returns>The newly created Span with the elements transformed by the predicate.</returns>
+        public Span<TResult> Select<TResult>(Func<TSource, TResult> predicate)
         {
-            array[index] = predicate.Invoke(x);
-            index++;
-        });
+            InvalidOperationException.ThrowIfSpanIsEmpty(source);
+            ArgumentNullException.ThrowIfNull(predicate);
+            
+            TResult[] array = new  TResult[source.Length];
+        
+            int index = 0;
+        
+            source.ForEach(x =>
+            {
+                array[index] = predicate.Invoke(x);
+                index++;
+            });
 
-        return new Span<TResult>(array);
+            return new Span<TResult>(array);
+        }
     }
 
-    /// <summary>
-    /// Transforms elements of a <see cref="ReadOnlySpan{T}"/> according to the defined predicate.
-    /// </summary>
     /// <param name="source">The <see cref="ReadOnlySpan{T}"/> to transform.</param>
-    /// <param name="predicate">The function to apply to each element of the <see cref="ReadOnlySpan{T}"/>.</param>
     /// <typeparam name="TSource">The type of elements in the source <see cref="ReadOnlySpan{T}"/>.</typeparam>
-    /// <typeparam name="TResult">The type of elements in the resulting <see cref="ReadOnlySpan{T}"/> after transformation.</typeparam>
-    /// <returns>A new <see cref="ReadOnlySpan{T}"/> with the elements transformed by the predicate.</returns>
-    public static ReadOnlySpan<TResult> Select<TSource, TResult>(
-#if NET8_0_OR_GREATER
-        [NotNull]
-#endif
-        this ReadOnlySpan<TSource> source,
-#if NET8_0_OR_GREATER
-        [NotNull]
-#endif
-        Func<TSource, TResult> predicate)
+    extension<TSource>(ReadOnlySpan<TSource> source)
     {
-        TResult[] array = new  TResult[source.Length];
-        
-        int index = 0;
-
-        foreach (TSource item in source)
+        /// <summary>
+        /// Transforms elements of a <see cref="ReadOnlySpan{T}"/> according to the defined predicate.
+        /// </summary>
+        /// <param name="predicate">The function to apply to each element of the <see cref="ReadOnlySpan{T}"/>.</param>
+        /// <typeparam name="TResult">The type of elements in the resulting <see cref="ReadOnlySpan{T}"/> after transformation.</typeparam>
+        /// <returns>A new <see cref="ReadOnlySpan{T}"/> with the elements transformed by the predicate.</returns>
+        public ReadOnlySpan<TResult> Select<TResult>(Func<TSource, TResult> predicate)
         {
-            array[index] = predicate.Invoke(item);
-            index++;
-        }
+            InvalidOperationException.ThrowIfSpanIsEmpty(source);
+            ArgumentNullException.ThrowIfNull(predicate);
+            
+            TResult[] array = new  TResult[source.Length];
+        
+            int index = 0;
 
-        return new Span<TResult>(array);
+            foreach (TSource item in source)
+            {
+                array[index] = predicate.Invoke(item);
+                index++;
+            }
+
+            return new Span<TResult>(array);
+        }
     }
 
-    /// <summary>
-    /// Transforms elements of a <see cref="Memory{T}"/> into a new <see cref="Memory{T}"/> using the defined predicate.
-    /// </summary>
     /// <param name="source">The source <see cref="Memory{T}"/> containing elements to be transformed.</param>
-    /// <param name="predicate">The transformation function to apply to each element in the <see cref="Memory{T}"/>.</param>
     /// <typeparam name="TSource">The type of elements in the source <see cref="Memory{T}"/>.</typeparam>
-    /// <typeparam name="TResult">The type of elements in the transformed <see cref="Memory{T}"/>.</typeparam>
-    /// <returns>A new <see cref="Memory{T}"/> containing the elements transformed by the predicate.</returns>
-    public static Memory<TResult> Select<TSource, TResult>(
-#if NET8_0_OR_GREATER
-        [NotNull]
-#endif
-        this Memory<TSource> source,
-#if NET8_0_OR_GREATER
-        [NotNull]
-#endif
-        Func<TSource, TResult> predicate)
+    extension<TSource>(Memory<TSource> source)
     {
-        TResult[] array = new  TResult[source.Length];
-        
-        int index = 0;
-        
-        source.ForEach(x =>
+        /// <summary>
+        /// Transforms elements of a <see cref="Memory{T}"/> into a new <see cref="Memory{T}"/> using the defined predicate.
+        /// </summary>
+        /// <param name="predicate">The transformation function to apply to each element in the <see cref="Memory{T}"/>.</param>
+        /// <typeparam name="TResult">The type of elements in the transformed <see cref="Memory{T}"/>.</typeparam>
+        /// <returns>A new <see cref="Memory{T}"/> containing the elements transformed by the predicate.</returns>
+        public Memory<TResult> Select<TResult>(Func<TSource, TResult> predicate)
         {
-            array[index] = predicate.Invoke(x);
-            index++;
-        });
+            InvalidOperationException.ThrowIfMemoryIsEmpty(source);
+            ArgumentNullException.ThrowIfNull(predicate);
+            
+            TResult[] array = new  TResult[source.Length];
         
-        return new Memory<TResult>(array);
+            int index = 0;
+        
+            source.ForEach(x =>
+            {
+                array[index] = predicate.Invoke(x);
+                index++;
+            });
+        
+            return new Memory<TResult>(array);
+        }
     }
 
-    /// <summary>
-    /// Transforms elements of a <see cref="ReadOnlyMemory{T}"/> according using the defined predicate.
-    /// </summary>
     /// <param name="source">The <see cref="ReadOnlyMemory{T}"/> containing the elements to be transformed.</param>
-    /// <param name="predicate">The transformation function to apply to each element in the <see cref="ReadOnlyMemory{T}"/></param>
     /// <typeparam name="TSource">The type of the elements in the source <see cref="ReadOnlyMemory{T}"/>.</typeparam>
-    /// <typeparam name="TResult">The type of the elements in the resulting <see cref="ReadOnlyMemory{T}"/>.</typeparam>
-    /// <returns>A new <see cref="ReadOnlyMemory{T}"/> containing the transformed elements.</returns>
-    public static ReadOnlyMemory<TResult> Select<TSource, TResult>(
-#if NET8_0_OR_GREATER
-        [NotNull]
-#endif
-        this ReadOnlyMemory<TSource> source,
-#if NET8_0_OR_GREATER
-        [NotNull]
-#endif
-        Func<TSource, TResult> predicate)
+    extension<TSource>(ReadOnlyMemory<TSource> source)
     {
-        TResult[] array = new TResult[source.Length];
-
-        int index = 0;
-
-        foreach (TSource item in source.Span)
+        /// <summary>
+        /// Transforms elements of a <see cref="ReadOnlyMemory{T}"/> according using the defined predicate.
+        /// </summary>
+        /// <param name="predicate">The transformation function to apply to each element in the <see cref="ReadOnlyMemory{T}"/></param>
+        /// <typeparam name="TResult">The type of the elements in the resulting <see cref="ReadOnlyMemory{T}"/>.</typeparam>
+        /// <returns>A new <see cref="ReadOnlyMemory{T}"/> containing the transformed elements.</returns>
+        public ReadOnlyMemory<TResult> Select<TResult>(Func<TSource, TResult> predicate)
         {
-            array[index] = predicate.Invoke(item);
-            index++;
-        }
+            InvalidOperationException.ThrowIfMemoryIsEmpty(source);
+            ArgumentNullException.ThrowIfNull(predicate);
+            
+            TResult[] array = new TResult[source.Length];
+
+            int index = 0;
+
+            foreach (TSource item in source.Span)
+            {
+                array[index] = predicate.Invoke(item);
+                index++;
+            }
         
-        return new Memory<TResult>(array);
+            return new Memory<TResult>(array);
+        }
     }
 }
