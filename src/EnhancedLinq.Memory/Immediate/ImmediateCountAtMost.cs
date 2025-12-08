44a67737f221a8e65bb66c5b35e8928aacd89471
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-12-08 10:46:17 +0000

    Update ImmediateCountAtMost.cs

--- src/EnhancedLinq.Memory/Immediate/ImmediateCountAtMost.cs
+++ src/EnhancedLinq.Memory/Immediate/ImmediateCountAtMost.cs
@@ -57,31 +57,47 @@ public static partial class EnhancedLinqMemoryImmediate
     }
 
 
-    /// <summary>
-    /// Determines whether there are at most a maximum number elements in the source <see cref="Span{T}"/> that satisfy the given condition.
-    /// </summary>
-    /// <param name="source">The source <see cref="Span{T}"/> to search through.</param>
-    /// <param name="predicate">The predicate condition to check elements against.</param>
-    /// <param name="countToLookFor">The maximum number of elements that can meet the condition.</param>
-    /// <typeparam name="T">The element type of the source <see cref="Span{T}"/>.</typeparam>
-    /// <returns>True if there are at most <paramref name="countToLookFor"/> number of elements that satisfy the condition, false otherwise.</returns>
-    public static bool CountAtMost<T>(this ReadOnlySpan<T> source, Func<T, bool> predicate,
-        int countToLookFor)
+    /// <param name="source">The source <see cref="ReadOnlySpan{T}"/> to search through.</param>
+    /// <typeparam name="T">The element type of the source <see cref="ReadOnlySpan{T}"/>.</typeparam>
+    extension<T>(ReadOnlySpan<T> source)
     {
-        InvalidOperationException.ThrowIfSpanIsEmpty(source);
-        ArgumentOutOfRangeException.ThrowIfNegative(countToLookFor);
-        
-        int currentCount = 0;
-
-        foreach (T obj in source)
+        /// <summary>
+        /// Determines whether there are at most a maximum number of elements in the source <see cref="ReadOnlySpan{T}"/>.
+        /// </summary>
+        /// <param name="countToLookFor">The maximum number of elements that can meet the condition.</param>
+        /// <returns>True if there are at most <paramref name="countToLookFor"/> number of elements, false otherwise.</returns>
+        public bool CountAtMost(int countToLookFor)
         {
-            if (predicate(obj))
-                currentCount += 1;
+            InvalidOperationException.ThrowIfSpanIsEmpty(source);
+            ArgumentOutOfRangeException.ThrowIfNegative(countToLookFor);
             
-            if(currentCount >= countToLookFor)
-                return false;
+            return source.Length <= countToLookFor;
         }
+        
+        /// <summary>
+        /// Determines whether there are at most a maximum number elements in the source <see cref="Span{T}"/> that satisfy the given condition.
+        /// </summary>
+        /// <param name="predicate">The predicate condition to check elements against.</param>
+        /// <param name="countToLookFor">The maximum number of elements that can meet the condition.</param>
+        /// <returns>True if there are at most <paramref name="countToLookFor"/> number of elements that satisfy the condition, false otherwise.</returns>
+        public bool CountAtMost(Func<T, bool> predicate,
+            int countToLookFor)
+        {
+            InvalidOperationException.ThrowIfSpanIsEmpty(source);
+            ArgumentOutOfRangeException.ThrowIfNegative(countToLookFor);
+        
+            int currentCount = 0;
 
-        return true;
+            foreach (T obj in source)
+            {
+                if (predicate(obj))
+                    currentCount += 1;
+            
+                if(currentCount >= countToLookFor)
+                    return false;
+            }
+
+            return true;
+        }
     }
 }
