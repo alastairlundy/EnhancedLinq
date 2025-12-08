d5af29fbc331e597101a992bf2e4d1b2bfbe0283
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-12-08 10:32:15 +0000

    add ReadOnlySpan Except

--- src/EnhancedLinq.Memory/Immediate/Linq/ImmediateExcept.cs
+++ src/EnhancedLinq.Memory/Immediate/Linq/ImmediateExcept.cs
@@ -22,6 +22,50 @@ public static partial class EnhancedLinqMemoryImmediate
         /// <returns>A new Span with all the elements of Span One and Span Two that were not in the other Span.</returns>
         public Span<T> Except(Span<T> second)
         {
+            InvalidOperationException.ThrowIfSpanIsEmpty(first);
+            InvalidOperationException.ThrowIfSpanIsEmpty(second);
+            
+            T[] output = new  T[first.Length + second.Length];
+            int index = 0;
+
+            foreach (T item in first)
+            {
+                if (second.Contains(item) == false)
+                {
+                    output[index] = item;
+                    index++;
+                }
+            }
+
+            foreach (T item in second)
+            {
+                if(first.Contains(item) == false)
+                {
+                    output[index] = item;
+                    index++;
+                }
+            }
+        
+            Array.Resize(ref output, index);
+
+            return new Span<T>(output);
+        }
+    }
+    
+    /// <param name="first">The first <see cref="ReadOnlySpan{T}"/> to search.</param>
+    /// <typeparam name="T">The type of items stored in the <see cref="ReadOnlySpan{T}"/>.</typeparam>
+    extension<T>(ReadOnlySpan<T> first) where T : IEquatable<T>
+    {
+        /// <summary>
+        /// Returns a new <see cref="ReadOnlySpan{T}"/> with all the elements of two <see cref="ReadOnlySpan{T}"/> objects that are only in one <see cref="ReadOnlySpan{T}"/> and not the other.
+        /// </summary>
+        /// <param name="second">The second <see cref="ReadOnlySpan{T}"/> to search.</param>
+        /// <returns>A new <see cref="ReadOnlySpan{T}"/> with all the elements of <see cref="ReadOnlySpan{T}"/> One and <see cref="ReadOnlySpan{T}"/> Two that were not in the other <see cref="ReadOnlySpan{T}"/>.</returns>
+        public ReadOnlySpan<T> Except(ReadOnlySpan<T> second)
+        {
+            InvalidOperationException.ThrowIfSpanIsEmpty(first);
+            InvalidOperationException.ThrowIfSpanIsEmpty(second);
+            
             T[] output = new  T[first.Length + second.Length];
             int index = 0;
 
