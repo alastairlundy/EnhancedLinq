3b7ffd436fe7ea9672014cfb9875ed2bfb3d9404
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-12-08 13:32:27 +0000

    add read only span CountAtLeast extensions

--- src/EnhancedLinq.Memory/Immediate/ImmediateCountAtLeast.cs
+++ src/EnhancedLinq.Memory/Immediate/ImmediateCountAtLeast.cs
@@ -55,4 +55,50 @@ public static partial class EnhancedLinqMemoryImmediate
             return false;
         }
     }
+    
+    /// <param name="source">The source <see cref="ReadOnlySpan{T}"/>.</param>
+    /// <typeparam name="T">The element type in the source <see cref="ReadOnlySpan{T}"/>.</typeparam>
+    extension<T>(ReadOnlySpan<T> source)
+    {
+        /// <summary>
+        /// Determines whether there are at least a specified number of elements in the <see cref="ReadOnlySpan{T}"/>.
+        /// </summary>
+        /// <param name="countToLookFor">The minimum count to look for.</param>
+        /// <returns><c>true</c> if there is at least the specified number of elements in the <see cref="ReadOnlySpan{T}"/>; otherwise, <c>false</c>.</returns>
+        public bool CountAtLeast(int countToLookFor)
+        {
+            InvalidOperationException.ThrowIfSpanIsEmpty(source);
+            ArgumentOutOfRangeException.ThrowIfNegative(countToLookFor);
+            
+            return source.Length >= countToLookFor;
+        }
+
+        /// <summary>
+        /// Determines whether there are at least a specified number of elements in the <see cref="ReadOnlySpan{T}"/> that meet a given condition.
+        /// </summary>
+        /// <param name="predicate">The predicate condition to check elements against.</param>
+        /// <param name="countToLookFor">The minimum count to look for.</param>
+        /// <returns><c>true</c> if there are at least the specified number of elements in the <see cref="ReadOnlySpan{T}"/>
+        /// that meet the condition; otherwise, <c>false</c>.</returns>
+        public bool CountAtLeast(Func<T, bool> predicate,
+            int countToLookFor)
+        {
+            InvalidOperationException.ThrowIfSpanIsEmpty(source);
+            ArgumentOutOfRangeException.ThrowIfNegative(countToLookFor);
+            ArgumentNullException.ThrowIfNull(predicate);
+
+            int currentCount = 0;
+
+            foreach (T obj in source)
+            {
+                if (predicate(obj))
+                    currentCount += 1;
+            
+                if(currentCount >= countToLookFor)
+                    return true;
+            }
+
+            return false;
+        }
+    }
 }
