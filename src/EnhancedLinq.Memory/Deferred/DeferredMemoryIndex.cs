f1b38ff04f1ff2168e1dc762c527bd3fc17ea8c6
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-12-13 04:06:21 +0000

    add Deferred Memory Index

---
+++ src/EnhancedLinq.Memory/Deferred/DeferredMemoryIndex.cs
@@ -0,0 +1,67 @@
+/*
+    EnhancedLinq.Memory
+    Copyright (c) 2025 Alastair Lundy
+    
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at https://mozilla.org/MPL/2.0/. 
+*/
+
+using System.Linq;
+
+namespace EnhancedLinq.Memory.Deferred;
+
+public static partial class EnhancedLinqMemoryDeferred
+{
+    /// <param name="memory">The <see cref="Memory{T}"/> whose indices will be generated.</param>
+    /// <typeparam name="TSource">The type of the elements in the <see cref="Memory{T}"/>.</typeparam>
+    extension<TSource>(Memory<TSource> memory)
+    {
+        /// <summary>
+        /// Returns a sequence of indices of all elements within the given <see cref="Memory{T}"/>, starting from an initial index of zero.
+        /// </summary>
+        /// <returns>A sequence of integers representing the indices of the elements in the <see cref="Memory{T}"/> starting from index zero.</returns>
+        /// <exception cref="InvalidOperationException">Thrown when the <see cref="Memory{T}"/> is empty.</exception>
+        public IEnumerable<(int Index, TSource Item)> Index()
+        { 
+            InvalidOperationException.ThrowIfMemoryIsEmpty(memory);
+            
+            int index = 0;
+
+            return memory.AsEnumerable()
+                .Select(x =>
+                {
+                    int tempIndex = index;
+                    index++;
+
+                    return (tempIndex, x);
+                });
+        }
+    }
+    
+    /// <param name="memory">The <see cref="ReadOnlyMemory{T}"/> whose indices will be generated.</param>
+    /// <typeparam name="TSource">The type of the elements in the <see cref="ReadOnlyMemory{T}"/>.</typeparam>
+    extension<TSource>(ReadOnlyMemory<TSource> memory)
+    {
+        /// <summary>
+        /// Returns a sequence of indices of all elements within the given <see cref="ReadOnlyMemory{T}"/>, starting from an initial index of zero.
+        /// </summary>
+        /// <returns>A sequence of integers representing the indices of the elements in the <see cref="ReadOnlyMemory{T}"/> starting from index zero.</returns>
+        /// <exception cref="InvalidOperationException">Thrown when the <see cref="ReadOnlyMemory{T}"/> is empty.</exception>
+        public IEnumerable<(int Index, TSource Item)> Index()
+        { 
+            InvalidOperationException.ThrowIfMemoryIsEmpty(memory);
+            
+            int index = 0;
+
+            return memory.AsEnumerable()
+                .Select(x =>
+                {
+                    int tempIndex = index;
+                    index++;
+
+                    return (tempIndex, x);
+                });
+        }
+    }
+}
