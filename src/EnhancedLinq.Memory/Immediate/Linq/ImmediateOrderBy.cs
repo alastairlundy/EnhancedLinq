ad0bd694cfe142b8fd2fae525e9f8e531f7732cc
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-12-13 04:07:08 +0000

    Create ImmediateOrderBy.cs

---
+++ src/EnhancedLinq.Memory/Immediate/Linq/ImmediateOrderBy.cs
@@ -0,0 +1,54 @@
+/*
+    EnhancedLinq.Memory
+    Copyright (c) 2025 Alastair Lundy
+    
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at https://mozilla.org/MPL/2.0/. 
+*/
+
+namespace EnhancedLinq.Memory.Immediate;
+
+public static partial class EnhancedLinqMemoryImmediate
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="source"></param>
+    /// <typeparam name="TSource">The type of the elements of the source Span.</typeparam>
+    extension<TSource>(Span<TSource> source)
+    {
+#if NET8_0_OR_GREATER
+        /// <summary>
+        /// Sorts the elements of a Span in ascending order by using a specified key selector.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the key returned by the key selector function.</typeparam>
+        /// <param name="predicate">The function to extract the key for each element.</param>
+        /// <returns>A new Span containing the sorted elements.</returns>
+        public Span<TSource> OrderBy<TKey>(Func<TSource, TKey> predicate)
+            => OrderBy(source, predicate, Comparer<TKey>.Default);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <param name="comparer"></param>
+        /// <typeparam name="TKey"></typeparam>
+        /// <returns></returns>
+        public Span<TSource> OrderBy<TKey>(Func<TSource, TKey> predicate, IComparer<TKey>? comparer)
+        {
+            InvalidOperationException.ThrowIfSpanIsEmpty(source);
+            ArgumentNullException.ThrowIfNull(predicate);
+            
+            comparer ??= Comparer<TKey>.Default;
+
+            Span<(TSource Item, TKey Key)> keyItemPairs = source.Select(s => (s, predicate(s)));
+              
+            keyItemPairs.Sort((left, right) =>
+                comparer.Compare(left.Key, right.Key));
+
+            return keyItemPairs.Select(p => p.Item);
+        }
+#endif
+    }
+}
