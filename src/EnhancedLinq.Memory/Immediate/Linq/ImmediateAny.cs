f60d0dfac1fa0680e4d037e51103d1d3e1668ea1
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-12-23 12:16:34 +0000

    add Memory<T> and ReadOnlyMemory<T> immediate Any

--- src/EnhancedLinq.Memory/Immediate/Linq/ImmediateAny.cs
+++ src/EnhancedLinq.Memory/Immediate/Linq/ImmediateAny.cs
@@ -8,6 +8,7 @@
     */
 
 using System.Linq;
+using EnhancedLinq.Memory.Deferred;
 
 namespace EnhancedLinq.Memory.Immediate;
 
@@ -66,6 +67,70 @@ public static partial class EnhancedLinqMemoryImmediate
             ArgumentNullException.ThrowIfNull(predicate);
             
             ReadOnlySpan<bool> groups = (from c in target
+                group c by predicate.Invoke(c)
+                into g
+                where g.Key
+                select g.Any());
+
+            bool? result = groups.FirstOrDefault();
+
+            return result ?? false;
+        }
+    }
+
+    /// <param name="target">The <see cref="Memory{T}"/> to be searched.</param>
+    /// <typeparam name="T">The type of items stored in the <see cref="Memory{T}"/>.</typeparam>
+    extension<T>(Memory<T> target)
+    {
+        /// <summary>
+        /// Returns whether there are any items in the <see cref="Memory{T}"/>.
+        /// </summary>
+        /// <returns></returns>
+        public bool Any() => target.Length > 0;
+
+        /// <summary>
+        /// Returns whether any item in a <see cref="Memory{T}"/> matches the predicate condition.
+        /// </summary>
+        /// <param name="predicate">The predicate func to be invoked on each item in the <see cref="Memory{T}"/>.</param>
+        /// <returns>True if any item in the <see cref="Memory{T}"/> matches the predicate; false otherwise.</returns>
+        public bool Any(Func<T, bool> predicate)
+        {
+            InvalidOperationException.ThrowIfMemoryIsEmpty(target);
+            ArgumentNullException.ThrowIfNull(predicate);
+            
+            IEnumerable<bool> groups = (from c in target
+                group c by predicate.Invoke(c)
+                into g
+                where g.Key
+                select g.Any());
+
+            bool? result = groups.FirstOrDefault();
+
+            return result ?? false;
+        }
+    }
+    
+    /// <param name="target">The <see cref="ReadOnlyMemory{T}"/> to be searched.</param>
+    /// <typeparam name="T">The type of items stored in the <see cref="ReadOnlyMemory{T}"/>.</typeparam>
+    extension<T>(ReadOnlyMemory<T> target)
+    {
+        /// <summary>
+        /// Returns whether there are any items in the <see cref="ReadOnlyMemory{T}"/>.
+        /// </summary>
+        /// <returns></returns>
+        public bool Any() => target.Length > 0;
+
+        /// <summary>
+        /// Returns whether any item in a <see cref="ReadOnlyMemory{T}"/> matches the predicate condition.
+        /// </summary>
+        /// <param name="predicate">The predicate func to be invoked on each item in the <see cref="ReadOnlyMemory{T}"/>.</param>
+        /// <returns>True if any item in the <see cref="ReadOnlyMemory{T}"/> matches the predicate; false otherwise.</returns>
+        public bool Any(Func<T, bool> predicate)
+        {
+            InvalidOperationException.ThrowIfMemoryIsEmpty(target);
+            ArgumentNullException.ThrowIfNull(predicate);
+            
+            IEnumerable<bool> groups = (from c in target
                 group c by predicate.Invoke(c)
                 into g
                 where g.Key
