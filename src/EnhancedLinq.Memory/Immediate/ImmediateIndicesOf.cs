f0d099f5771771a6f3a013c0414d8ea5e6baa5cc
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-12-23 07:43:28 +0000

    add ReadOnlySpan<T> IndicesOf

--- src/EnhancedLinq.Memory/Immediate/ImmediateIndicesOf.cs
+++ src/EnhancedLinq.Memory/Immediate/ImmediateIndicesOf.cs
@@ -9,20 +9,17 @@
 
 namespace EnhancedLinq.Memory.Immediate;
 
-/// <summary>
-/// 
-/// </summary>
 public static partial class EnhancedLinqMemoryImmediate
 {
-    /// <param name="source">The initial span to search.</param>
-    /// <typeparam name="T">The type of elements within the span.</typeparam>
+    /// <param name="source">The <see cref="Span{T}"/> to search.</param>
+    /// <typeparam name="T">The type of elements within the <see cref="Span{T}"/>.</typeparam>
     extension<T>(Span<T> source) where T : notnull
     {
         /// <summary>
-        /// Gets a collection of indices within the given span where the specified value occurs.
+        /// Gets a collection of indices within the given <see cref="Span{T}"/> where the specified value occurs.
         /// </summary>
-        /// <param name="item">The value to find in the span.</param>
-        /// <returns>A collection of indices that represent the occurrences of item in span.</returns>
+        /// <param name="item">The value to find in the <see cref="Span{T}"/>.</param>
+        /// <returns>A collection of indices that represent the occurrences of item in <see cref="Span{T}"/></returns>
         public ICollection<int> IndicesOf(T item)
         {
             InvalidOperationException.ThrowIfSpanIsEmpty(source);
@@ -41,10 +38,58 @@ public static partial class EnhancedLinqMemoryImmediate
         }
 
         /// <summary>
-        /// Gets a collection of indices within the given span where items match the predicate condition.
+        /// Gets a collection of indices within the given <see cref="Span{T}"/> where items match the predicate condition.
         /// </summary>
-        /// <param name="predicate"></param>
-        /// <returns>A collection of indices that represent all items in the span that match the predicate.</returns>
+        /// <param name="predicate">The predicate to use to select items to retrieve the index of.</param>
+        /// <returns>A collection of indices that represent all items in the <see cref="Span{T}"/> that match the predicate.</returns>
+        public ICollection<int> IndicesOf(Func<T, bool> predicate)
+        {            
+            InvalidOperationException.ThrowIfSpanIsEmpty(source);
+            ArgumentNullException.ThrowIfNull(predicate);
+            
+            List<int> indices = new();
+        
+            for (int index = 0; index < source.Length; index++)
+            {
+                if(predicate(source[index]))
+                    indices.Add(index);
+            }
+        
+            return indices;
+        }
+    }
+
+    /// <param name="source">The <see cref="ReadOnlySpan{T}"/> to search.</param>
+    /// <typeparam name="T">The type of elements within the <see cref="ReadOnlySpan{T}"/>.</typeparam>
+    extension<T>(ReadOnlySpan<T> source) where T : notnull
+    {
+        /// <summary>
+        /// Gets a collection of indices within the given <see cref="ReadOnlySpan{T}"/> where the specified value occurs.
+        /// </summary>
+        /// <param name="item">The value to find in the <see cref="ReadOnlySpan{T}"/>.</param>
+        /// <returns>A collection of indices that represent the occurrences of item in <see cref="ReadOnlySpan{T}"/>.</returns>
+        public ICollection<int> IndicesOf(T item)
+        {
+            InvalidOperationException.ThrowIfSpanIsEmpty(source);
+
+            List<int> indices = new();
+
+            for (int index = 0; index < source.Length; index++)
+            {
+                if (item is not null && item.Equals(source[index]))
+                {
+                    indices.Add(index);
+                }
+            }
+        
+            return indices;
+        }
+
+        /// <summary>
+        /// Gets a collection of indices within the given <see cref="ReadOnlySpan{T}"/> where items match the predicate condition.
+        /// </summary>
+        /// <param name="predicate">The predicate to use to select items to retrieve the index of.</param>
+        /// <returns>A collection of indices that represent all items in the <see cref="ReadOnlySpan{T}"/> that match the predicate.</returns>
         public ICollection<int> IndicesOf(Func<T, bool> predicate)
         {            
             InvalidOperationException.ThrowIfSpanIsEmpty(source);
