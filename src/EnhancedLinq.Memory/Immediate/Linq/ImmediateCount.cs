c426dfe6076dea55373dfe67d433f9a3d80026b6
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-12-23 11:19:31 +0000

    add Memory<T> Count

--- src/EnhancedLinq.Memory/Immediate/Linq/ImmediateCount.cs
+++ src/EnhancedLinq.Memory/Immediate/Linq/ImmediateCount.cs
@@ -7,6 +7,8 @@
     file, You can obtain one at https://mozilla.org/MPL/2.0/. 
 */
 
+using EnhancedLinq.Memory.Deferred;
+
 namespace EnhancedLinq.Memory.Immediate;
 
 public static partial class EnhancedLinqMemoryImmediate
@@ -61,11 +63,8 @@ public static partial class EnhancedLinqMemoryImmediate
 #endif
     }
     
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="source"></param>
-    /// <typeparam name="TSource"></typeparam>
+    /// <param name="source">The <see cref="ReadOnlySpan{T}"/> to search.</param>
+    /// <typeparam name="TSource">The type of elements in the <see cref="ReadOnlySpan{T}"/>.</typeparam>
     extension<TSource>(ReadOnlySpan<TSource> source)
     {
         /// <summary>
@@ -104,6 +103,106 @@ public static partial class EnhancedLinqMemoryImmediate
             TNumber total = TNumber.Zero;
 
             foreach (TSource item in source)
+            {
+                if(predicate(item))
+                    total += TNumber.One;
+            }
+        
+            return total;
+        }
+#endif
+    }
+
+    /// <param name="source">The <see cref="Memory{T}"/> to search.</param>
+    /// <typeparam name="TSource">The type of elements in the <see cref="Memory{T}"/>.</typeparam>
+    extension<TSource>(Memory<TSource> source)
+    {
+        /// <summary>
+        /// Returns the number of elements in a given <see cref="Memory{T}"/> that satisfy a condition.
+        /// </summary>
+        /// <param name="predicate">A func that takes an element and returns a boolean indicating whether it should be counted.</param>
+        /// <returns>The number of elements that satisfy the predicate.</returns>
+        public int Count(Func<TSource, bool> predicate)
+        {
+            InvalidOperationException.ThrowIfMemoryIsEmpty(source);
+            ArgumentNullException.ThrowIfNull(predicate);
+            
+            int count = 0;
+
+            foreach (TSource item in source.AsEnumerable())
+            {
+                if (predicate(item)) 
+                    count++;
+            }
+        
+            return count;
+        }
+
+#if NET8_0_OR_GREATER
+        /// <summary>
+        /// Returns the number of elements in a given <see cref="Memory{T}"/> that satisfy a condition.
+        /// </summary>
+        /// <param name="predicate">A func that takes an element and returns a boolean indicating whether it should be counted.</param>
+        /// <typeparam name="TNumber">The numeric type that represents the type of numbers in the <see cref="Memory{T}"/>.</typeparam>
+        /// <returns>The number of elements that satisfy the predicate.</returns>
+        public TNumber Count<TNumber>(Func<TSource, bool> predicate) where TNumber : INumber<TNumber>
+        {
+            InvalidOperationException.ThrowIfMemoryIsEmpty(source);
+            ArgumentNullException.ThrowIfNull(predicate);
+            
+            TNumber total = TNumber.Zero;
+
+            foreach (TSource item in source.AsEnumerable())
+            {
+                if(predicate(item))
+                    total += TNumber.One;
+            }
+        
+            return total;
+        }
+#endif
+    }
+
+    /// <param name="source">The <see cref="ReadOnlyMemory{T}"/> to search.</param>
+    /// <typeparam name="TSource">The type of elements in the <see cref="ReadOnlyMemory{T}"/>.</typeparam>
+    extension<TSource>(ReadOnlyMemory<TSource> source)
+    {
+        /// <summary>
+        /// Returns the number of elements in a given <see cref="ReadOnlyMemory{T}"/> that satisfy a condition.
+        /// </summary>
+        /// <param name="predicate">A func that takes an element and returns a boolean indicating whether it should be counted.</param>
+        /// <returns>The number of elements that satisfy the predicate.</returns>
+        public int Count(Func<TSource, bool> predicate)
+        {
+            InvalidOperationException.ThrowIfMemoryIsEmpty(source);
+            ArgumentNullException.ThrowIfNull(predicate);
+            
+            int count = 0;
+
+            foreach (TSource item in source.AsEnumerable())
+            {
+                if (predicate(item)) 
+                    count++;
+            }
+        
+            return count;
+        }
+
+#if NET8_0_OR_GREATER
+        /// <summary>
+        /// Returns the number of elements in a given <see cref="ReadOnlyMemory{T}"/> that satisfy a condition.
+        /// </summary>
+        /// <param name="predicate">A func that takes an element and returns a boolean indicating whether it should be counted.</param>
+        /// <typeparam name="TNumber">The numeric type that represents the type of numbers in the <see cref="ReadOnlyMemory{T}"/>.</typeparam>
+        /// <returns>The number of elements that satisfy the predicate.</returns>
+        public TNumber Count<TNumber>(Func<TSource, bool> predicate) where TNumber : INumber<TNumber>
+        {
+            InvalidOperationException.ThrowIfMemoryIsEmpty(source);
+            ArgumentNullException.ThrowIfNull(predicate);
+            
+            TNumber total = TNumber.Zero;
+
+            foreach (TSource item in source.AsEnumerable())
             {
                 if(predicate(item))
                     total += TNumber.One;
