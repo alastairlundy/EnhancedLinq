685d25ee005fb1d4e471bb774011fedfef1daef7
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-02-04 16:13:09 +0000

    add Deferred IndicesOf Async

---
+++ src/EnhancedLinq.Async/Deferred/DeferredNIndicesOfAsync.cs
@@ -0,0 +1,80 @@
+﻿/*
+    EnhancedLinq.Async
+    Copyright (c) 2025-2026 Alastair Lundy
+    
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at https://mozilla.org/MPL/2.0/.
+*/
+
+using System.Linq;
+
+namespace EnhancedLinq.Async.Deferred;
+
+public static partial class EnhancedLinqAsyncDeferred
+{
+    /// <summary>
+    /// Extension methods for deferred asynchronous operations on <see cref="IAsyncEnumerable{T}"/>.
+    /// </summary>
+    extension<TSource>(IAsyncEnumerable<TSource> source)
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        public IAsyncEnumerable<int> FirstNIndicesOf(TSource target, int count)
+        {
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(count);
+            ArgumentNullException.ThrowIfNull(source);
+            
+            return source.IndicesOfAsync(target).Take(count);
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="selector"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        public IAsyncEnumerable<int> FirstNIndicesOf(Func<TSource, bool> selector, int count)
+        {
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(count);
+            ArgumentNullException.ThrowIfNull(source);
+            ArgumentNullException.ThrowIfNull(selector);
+
+            
+            return source.IndicesOfAsync(selector).Take(count);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        public IAsyncEnumerable<int> LastNIndicesOf(TSource target, int count)
+        {
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(count);
+            ArgumentNullException.ThrowIfNull(source);
+            
+            return source.IndicesOfAsync(target).TakeLast(count);
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="selector"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        public IAsyncEnumerable<int> LastNIndicesOf(Func<TSource, bool> selector, int count)
+        {
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(count);
+            ArgumentNullException.ThrowIfNull(selector);
+            ArgumentNullException.ThrowIfNull(source);
+            
+            return source.IndicesOfAsync(selector).TakeLast(count);
+        }
+    }
+}
---
+++ src/EnhancedLinq.Async/Deferred/Enumerators/GenericIndicesAsyncEnumerator.cs
@@ -0,0 +1,68 @@
+﻿/*
+    EnhancedLinq.Async
+    Copyright (c) 2025-2026 Alastair Lundy
+    
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at https://mozilla.org/MPL/2.0/.
+*/
+
+namespace EnhancedLinq.Async.Deferred.Enumerators;
+
+internal class GenericIndicesAsyncEnumerator<TSource> : IAsyncEnumerator<int>
+{
+    private readonly Func<TSource, bool> _predicate;
+
+    private readonly IAsyncEnumerator<TSource> _enumerator;
+
+    private int _state;
+    private int _index;
+    
+    internal GenericIndicesAsyncEnumerator(IAsyncEnumerable<TSource> source, Func<TSource, bool> predicate)
+    {
+        _predicate = predicate;
+        _enumerator = source.GetAsyncEnumerator();
+        _state = 1;
+        _index = 0;
+    }
+    
+    public void Reset()
+    {
+        throw new NotSupportedException();
+    }
+
+    public async ValueTask<bool> MoveNextAsync()
+    {
+        if (_state == 1)
+        {
+            try
+            {
+                while (await _enumerator.MoveNextAsync())
+                {
+                    if (_predicate(_enumerator.Current))
+                    {
+                        Current = _index;
+                        return true;
+                    }
+
+                    _index++;
+                }
+            }
+            finally
+            {
+                await DisposeAsync();
+                _state = -1;
+            }
+        }
+        
+        await DisposeAsync();
+        return false;
+    }
+
+    public int Current { get; private set; }
+
+    public async ValueTask DisposeAsync()
+    {
+        await _enumerator.DisposeAsync().ConfigureAwait(false);
+    }
+}
