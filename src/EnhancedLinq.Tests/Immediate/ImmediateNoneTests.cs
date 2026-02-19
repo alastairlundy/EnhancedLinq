64a269ed809009d749aa6b85ebb26cf5bf181ed2
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-02-19 17:45:36 +0000

    add initial tests for None method

---
+++ src/EnhancedLinq.Tests/Immediate/ImmediateNoneTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ExtendedLinq.Tests.Immediate;
+
+public class ImmediateNoneTests
+{
+    private readonly Faker _faker = new();
+    
+    [Test]
+    public async Task None_Empty_Enumerable_Throws()
+    {
+        await Assert.ThrowsAsync<InvalidOperationException>(() => Task.FromResult(Enumerable.Empty<string>()
+            .None(s => s.Length > 0)));
+    }
+
+    [Test]
+    public async Task None_numerable_NoMatches_ReturnsTrue()
+    {
+        IList<string> source = _faker.Make(Random.Shared.Next(1, 10), _ => _faker.Lorem.Word());
+
+        bool expected = source.None(s => s.Length == 0);
+        
+        await Assert.That(expected)
+            .IsTrue();
+    }
+
+    [Test]
+    public async Task None_Enumerable_Matches_ReturnsFalse()
+    {
+        IEnumerable<DateTime> source = _faker.MakeLazy(Random.Shared.Next(1, 10), _ => _faker.Date.Between(DateTime.Today.AddDays(1), DateTime.Today.AddDays(1000)));
+
+        bool expected = source.None(d => d.Date > DateTime.Today.Date);
+        
+        await Assert.That(expected)
+            .IsFalse();
+    }
+}
