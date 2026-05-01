7a48467639f974fb1b278c6e5d068712c388c9b5
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-05-01 17:16:19 +0100

    Add async FindDuplicates extensions
    Introduce duplicate-detection for IAsyncEnumerable: adds DeferredDuplicatesExtensions.cs with FindDuplicates overloads (default and custom IEqualityComparer)

---
+++ src/EnhancedLinq.Async/Deferred/Enumerators/AsyncDuplicatesEnumerator.cs
@@ -0,0 +1,47 @@
+﻿namespace EnhancedLinq.Async.Deferred.Enumerators;
+
+internal class AsyncDuplicatesEnumerator<TSource> : IAsyncEnumerator<TSource>
+{
+    private readonly IAsyncEnumerator<TSource> _enumerator;
+
+    private int _state = 1;
+
+    private readonly HashSet<TSource> _hashSet;
+
+    public AsyncDuplicatesEnumerator(IAsyncEnumerable<TSource> source, IEqualityComparer<TSource> comparer)
+    {
+        _hashSet = new HashSet<TSource>(comparer);
+        
+        _enumerator = source.GetAsyncEnumerator();
+        Current = _enumerator.Current;
+    }
+    
+    public async ValueTask DisposeAsync()
+    {
+        await _enumerator.DisposeAsync().ConfigureAwait(false);
+    }
+
+    public async ValueTask<bool> MoveNextAsync()
+    {
+        if (_state == 1)
+        {
+            while (await _enumerator.MoveNextAsync().ConfigureAwait(false))
+            {
+                bool isDuplicate = _hashSet.Add(_enumerator.Current);
+
+                if (isDuplicate)
+                {
+                    Current = _enumerator.Current;
+                    return true;
+                }
+            }
+
+            _state = 2;
+        }
+            
+        await DisposeAsync().ConfigureAwait(false);
+        return false;
+    }
+
+    public TSource Current { get; private set; }
+}
